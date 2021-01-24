11a5fb89c0fba6a3531808b4a61011bc93f987cb
Ethan Shipulski <EShipulski@sunrise-labs.local>
2021-01-24 00:21:35 -0500

    updated logic

--- knobTest/knobTest.ino
+++ knobTest/knobTest.ino
@@ -1,12 +1,16 @@
 // define how the pins are going to be used
-const uint8_t PinSDA = 0;                                                       // I2C data
-const uint8_t PinSCL = 2;                                                       // I2C clock
-const uint8_t PinEncoderA = 1;                                                   // set this to the pin connected to encoder-A
-const uint8_t PinEncoderB = 3;                                                   // set this to the pin connected to encoder-B
+#include <Wire.h>                                                               // Global for amplifier
+#define MAX9744_I2CADDR 0x4B
+
+const uint8_t PinSDA = 0;                                                        // I2C data
+const uint8_t PinSCL = 2;                                                        // I2C clock
+const uint8_t PinEncoderA = 3;                                                   // set this to the pin connected to encoder-A
+const uint8_t PinEncoderB = 4;                                                   // set this to the pin connected to encoder-B
 
 // define constants
-const uint8_t KnobGain = 1;                                                      // increase this to make volume chsnge quicker
-uint8_t position = 0;                                                            // initial knob position (volume)
+const uint8_t KnobGain = 1;                                                      // increase this to make volume change quicker
+uint8_t position = 0;// initial knob position (volume)
+int8_t thevol = 0;
 
 /**
  * Setup()
@@ -24,7 +28,7 @@ void setup()
 
 /**
  * loop()_
- * This gets called over and over by the arduino librsry
+ * This gets called over and over by the arduino library
  */
 void loop() 
 {
@@ -38,32 +42,36 @@ void loop()
 
 /**
  * IsrEncoderA()
- * This gets called on every chsnging edge of encoder A
+ * This gets called on every changing edge of encoder A
  */
 void IsrEncoderA(void)
 {
-   if (digitalRead(PinEncoderB) == 0)
+   if (digitalRead(PinEncoderB) == digitalRead(PinEncoderA))                    // If pins are same, turning CCW
    {
-      position = position - KnobGain;     // CCW motion of the knob
+      position = position - KnobGain;   // CCW motion of the knob
+      Serial.println("down A");
    }
    else
    {
        position = position + KnobGain;    // CW motion of the knob
+       Serial.println("up A"); 
    }
 }
 
 /**
  * IsrEncoderB()
- * This gets called on every chsnging edge of encoder B
+ * This gets called on every changing edge of encoder B
  */
 void IsrEncoderB(void)
 {
-     if (digitalRead(PinEncoderA) == 0)
+     if (digitalRead(PinEncoderA) == digitalRead(PinEncoderB))                 // If pins are same, turning CW
    {
-      position = position - KnobGain;     // CCW motion of the knob
+      position = position + KnobGain;     // CW motion of the knob
+      Serial.println("up B");
    }
    else
    {
-       position = position + KnobGain;    // CW motion of the knob
+       position = position - KnobGain;    // CCW motion of the knob
+       Serial.println("down B");
    }
 }
